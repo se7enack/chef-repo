df759f4cf29b6c20cd129aadb3c7b89219915e51
Stephen Burke <steburke71@hotmail.com>
2016-10-08 19:27:20 -0400

    initial commit

---
+++ cookbooks/motd/metadata.rb
@@ -0,0 +1,7 @@
+name             'motd'
+maintainer       'Stephen Burke'
+maintainer_email 'steburke71@hotmail.com'
+license          'All rights reserved'
+description      'Installs/Configures motd'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.0'
